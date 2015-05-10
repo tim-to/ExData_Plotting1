26e37e73f9eb9d7f35c8e356a4263361671f4e31
Tim To <tim.to.van@gmail.com>
2015-05-10 01:55:57 -0700

    adding plot 1 and 2 R scripts and png files

---
+++ plot2.R
@@ -0,0 +1,16 @@
+
+inFile<-"household_power_consumption.txt"
+
+# Read in the file. The line number to skip been determine by first looking in the
+# data file and searching for the start date of 1/2/2007
+household<-read.table(inFile, sep=";", header=F, skip=66637, nrow=2880, stringsAsFactors=F)
+# Set the column names by reading the first line then copy over the column names.
+dataname<-read.table(inFile, sep=";", header=T,  nrow=1)
+colnames(household) <-colnames(dataname)
+# Combine the date and time columns to form the complete date.
+household$Date <-strptime(paste(household$Date, household$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
+
+# plot the data to a png device
+png(filename="plot2.png",width=480,height=480)
+plot(household$Date, household$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
